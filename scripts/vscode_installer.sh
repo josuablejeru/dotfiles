e9590b69550d39677882dfaccdcde0ae9cd4dc42
Josua Blejeru <josuablejeru@Josuas-MBP.fritz.box>
2019-07-25 12:05:16 +0200

    move all scripts to a dir

---
+++ scripts/vscode_installer.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+# vscode installer script
+# pleas install the "code" tool to your path first to use this script
+
+echo "start vscode installer"
+
+
+code --install-extension wallabyjs.quokka-vscode
+code --install-extension eamodio.gitlens
+code --install-extension vscodevim.vim
+code --install-extension vscode-icons-team.vscode-icons
+code --install-extension dracula-theme.theme-dracula
+code --install-extension ms-python.python
+
+echo -e "Installer ist \e[5mFertig!"
