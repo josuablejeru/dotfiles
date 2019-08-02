09958b68f7bac8435d78e0b95434dffcbe2a0e5e
Josua Blejeru <josua.blejeru@gmail.com>
2019-08-02 19:03:21 +0300

    add terraform plugin

--- scripts/vscode_installer.sh
+++ scripts/vscode_installer.sh
@@ -4,12 +4,13 @@
 
 echo "start vscode installer"
 
-
 code --install-extension wallabyjs.quokka-vscode
 code --install-extension eamodio.gitlens
 code --install-extension vscodevim.vim
 code --install-extension vscode-icons-team.vscode-icons
 code --install-extension dracula-theme.theme-dracula
 code --install-extension ms-python.python
+code --install-extension ms-azuretools.vscode-docker 
+code --install-extension mauve.terraform
 
 echo -e "Installer ist \e[5mFertig!"
