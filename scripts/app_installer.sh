bfe745fb89c1f98d2295c4e8ca079f3967d8e25e
Josua Blejeru <josua.blejeru@gmail.com>
2020-12-06 22:18:47 +0100

    append jenv package

--- scripts/app_installer.sh
+++ scripts/app_installer.sh
@@ -13,8 +13,8 @@ APPSTORE=(
     441258766  # Magnet
     407963104  # Pixelmator
     1228059008 # ChatMate for WhatsApp
-	1333542190 # 1passsword
-	1333542190 # 1passsword
+		1333542190 # 1passsword
+		1333542190 # 1passsword
 )
 
 echo "Install apps from Appstore"
@@ -30,6 +30,7 @@ CASKS=(
     slack
     vlc
     postman
+		jenv
 )
 
 echo "Installing cask apps..."
