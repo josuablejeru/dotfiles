79a557732cfdbb46ed275e2f160bc0dc09e77fbb
Josua Blejeru <37913833+josuablejeru@users.noreply.github.com>
2021-03-12 00:14:53 +0100

    add p10k plugin

--- scripts/oh_my_zsh_plugins.sh
+++ scripts/oh_my_zsh_plugins.sh
@@ -1,3 +1,4 @@
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 source .zshrc
