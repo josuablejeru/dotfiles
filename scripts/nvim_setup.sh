fc99e9d6afe5c1e3c5f1d18c59b95e1071d019ba
Josua Blejeru <37913833+josuablejeru@users.noreply.github.com>
2021-03-13 13:58:35 +0100

    include some language support

--- scripts/nvim_setup.sh
+++ scripts/nvim_setup.sh
@@ -6,13 +6,16 @@ echo "setup nvim"
 # install neovim
 brew install neovim
 
-# simlink all files
+# simlink the directory
 ln -s ~/dotfiles/nvim ~/.config/nvim
 
 
 # Download vim-plug
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+echo "install language helpers"
+pip install pyenv
+yarn global add neovim
 
 # Run :Plug<comand>
 echo "> install plugins with `:PlugInstall`"
